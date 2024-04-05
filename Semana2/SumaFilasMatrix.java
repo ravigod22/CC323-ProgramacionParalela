2b4d783ad61483d51094aeba13fc78fccf3b9a08
Manuel <110348172+ravigod22@users.noreply.github.com>
2024-04-05 09:13:45 -0500

    Update SumaFilasMatrix.java

--- Semana2/SumaFilasMatrix.java
+++ Semana2/SumaFilasMatrix.java
@@ -5,6 +5,7 @@ public class SumaFilasMatrix {
     private static final int NUM_THREADS = 3;
     private static volatile int currentRow = 0;
     private static volatile int[][] MTX = new int[N][N];
+    private static Thread[] threads = new Thread[NUM_THREADS];
     private static final AtomicInteger[] STATES = new AtomicInteger[NUM_THREADS];
 
     public static void LoadMatrix() {
@@ -40,8 +41,6 @@ public class SumaFilasMatrix {
         ShowMatrix();
         System.out.println();
         System.out.println("Resultado en serial => " + SumSerial());
-        Thread[] threads = new Thread[NUM_THREADS];
-
         // Inicializamos los elementos del arreglo STATES
         for (int i = 0; i < NUM_THREADS; i++) {
             STATES[i] = new AtomicInteger(0);
